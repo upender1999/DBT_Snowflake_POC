a0e4ebd4a9fb8e2317b3fcfa11e0ae1ed3c3c5a2
upender1999 <168r1a0471@gmail.com>
2021-11-23 17:06:02 +0530

    commit on 23/11

---
+++ DBT_INSTALL.sh
@@ -0,0 +1,12 @@
+#Install the latest pip
+pip install dbt --user
+echo $(pwd)
+cd _upender1999_DBT_POC
+dbt deps
+dbt compile
+dbt seed
+dbt run
+dbt test
+dbt docs generate
+dbt docs serve --port 8001
+# End of the program
